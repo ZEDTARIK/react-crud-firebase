1a95fe0dc8ee0aa6f32d5fd0a62f5a1971b52f23
zouhair <ettarak.zouhair@gmail.com>
2020-08-24 22:41:05 +0100

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import * as firebase  from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDAFnsGzY9eGh4tPhYpNhPEJcdB-lYGQOg",
+    authDomain: "reactcrud-25bb1.firebaseapp.com",
+    databaseURL: "https://reactcrud-25bb1.firebaseio.com",
+    projectId: "reactcrud-25bb1",
+    storageBucket: "reactcrud-25bb1.appspot.com",
+    messagingSenderId: "778973603168",
+    appId: "1:778973603168:web:bfb9c9f8d4742e1dc82a05"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
